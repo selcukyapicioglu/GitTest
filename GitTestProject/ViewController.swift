d511ab700bb74a356a9a4948f240b32420047562
selcukyapicioglu <selcukyapicioglu@hotmail.com>
2020-06-01 16:57:15 +0300

    git'e yeni komut eklendi

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Apple on 1.06.2020.
+//  Copyright © 2020 Selcuk YAPICIOGLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+        print("git2")
+        print("git3")
+        print("git4")
+    }
+    
+
+
+}
+
